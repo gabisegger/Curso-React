fc41bfb1e906357f9fa99dcf4427ce7d37a23170
gabisegger <gabi.segger@gmail.com>
2022-03-15 12:08:43 -0300

    chore: working with props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Matheus'
-  const newName = name.toUpperCase()
+  const nome = 'Maria'
 
-  function sum (a, b) {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-  
   return (
     <div className="App">
-      <h1>Alterando o JSX</h1>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha Imagem" />
       <HelloWorld />
+      <SayMyName nome="Matheus" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa 
+        nome="Rodrigo" 
+        idade="28" 
+        profissao="Programador" 
+        foto="https://via.placeholder.com/150" 
+      />
     </div>
   );
 }
