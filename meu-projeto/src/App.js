4482169d3060f3c0f8c040f5b9435a56ba25ca53
gabisegger <gabi.segger@gmail.com>
2022-03-17 11:49:17 -0300

    chore: Events in React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import List from './components/List';
+import Form from './components/Form';
 
 function App() {
   const nome = 'Maria'
 
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <HelloWorld />
-      <SayMyName nome="Matheus" />
-      <SayMyName nome="João" />
-      <SayMyName nome={nome} />
-      <Pessoa 
-        nome="Rodrigo" 
-        idade="28" 
-        profissao="Programador" 
-        foto="https://via.placeholder.com/150" 
-      />
-      <List/>
+      <h1>Testando Eventos</h1>
+      <Form/>
     </div>
   );
 }
