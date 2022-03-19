c23e0e760d61b7cdc6fb0cb8892515376ff4d19a
gabisegger <gabi.segger@gmail.com>
2022-03-19 16:07:09 -0300

    fix:  Implementing React Router

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Contato from './pages/Contato'
 import Empresa from './pages/Empresa'
